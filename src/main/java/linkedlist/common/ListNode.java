b945c665b47913e17739c589e8c9bd293cdf12ac
zhouyuan <zhouyuanee@gmail.com>
2023-04-01 14:32:30 +0800

    Problem0019

--- src/main/java/linkedlist/common/ListNode.java
+++ src/main/java/linkedlist/common/ListNode.java
@@ -13,8 +13,16 @@ public class ListNode {
     public int val;
     public ListNode next;
 
+    public ListNode() {
+    }
+
     public ListNode(int x) {
         val = x;
         next = null;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
